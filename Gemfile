ea8203fb589695aead8d20abdf96e8a4b47551ad
piapugagermany <pipuge@gmail.com>
2020-01-25 22:21:14 -0300

    implementación de alertify

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ end
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'alertifyjs-rails'
 gem 'fog-aws'
 gem 'carrierwave'
 gem 'rails', '~> 5.1.7'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require alertify
+//= require alertify/confirm-modal
+//= require alertify/confirm-ujs
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
