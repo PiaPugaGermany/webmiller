9a27db0d287876887828852d8b96027e42a063f9
piapugagermany <pipuge@gmail.com>
2020-01-29 22:16:37 -0300

    arreglos front en vistas de usuarios

---
+++ app/admin/dashboard.rb
@@ -0,0 +1,39 @@
+ActiveAdmin.register_page "Dashboard" do
+  menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
+
+  content title: proc { I18n.t("active_admin.dashboard") } do
+      columns do
+        columns do
+          panel 'Últimos posts publicados' do
+            ul do
+              Post.last(5).map do |post|
+                li link_to(post.title, admin_post_path(post))
+
+              end
+            end
+          end
+        end
+        column do
+          panel 'Usuarios' do
+            ul do
+              li "Usuarios registrados: #{User.count}"
+              li "Administradores registrados: #{AdminUser.count}"
+            end
+          end
+        end
+        column do
+          panel 'Post' do
+            ul do
+              li "Post creados: #{Post.count}"
+              li "Comentarios creados: #{Comment.count}"
+            end
+          end
+
+        end
+      end
+
+    panel "Gráfico" do
+      render 'shared/chart'
+    end
+  end # content
+end
